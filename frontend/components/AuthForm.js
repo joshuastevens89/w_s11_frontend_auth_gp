57c6e8956778f56af620727ee1abd409a6ada7b2
Joshua Stevens <e2855845@student.dcccd.edu>
2024-11-13 02:59:34 -0600

    completed module 2 guided project

--- frontend/components/AuthForm.js
+++ frontend/components/AuthForm.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
-
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 export default function AuthForm() {
+  const navigate = useNavigate()
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value)
@@ -11,13 +14,30 @@ export default function AuthForm() {
     setPassword(event.target.value)
   }
   const handleSubmit = async (event) => {
+    event.preventDefault()
+    setError('')
+    try {
+      const { data } = await axios.post(
+        '/api/auth/login',
+        { username, password }
+      )
+      localStorage.setItem('token', data.token)
+      navigate('/cereals')
+
+    } catch (err) {
+      setError(
+        err?.response?.data?.message ||
+        'An error occured. Please try again.'
+      )
+    }
+
 
   }
 
   return (
     <div className="container">
       <div aria-live="polite"></div>
-      <div aria-live="assertive" style={{ color: 'red' }}></div>
+      <div aria-live="assertive" style={{ color: 'red' }}>{error}</div>
       <h3>Login Form</h3>
       <form onSubmit={handleSubmit}>
         <div>
